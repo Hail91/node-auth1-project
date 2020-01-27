a988c8ba59758307616b288d66abee688245930d
Aaron <aarongillies88@gmail.com>
2020-01-27 17:57:03 -0500

    Setup finished, MVP reached

---
+++ auth/auth-router.js
@@ -0,0 +1,88 @@
+const bc = require("bcryptjs");
+const router = require("express").Router();
+
+const Users = require("../users/users-model.js");
+
+router.get("/users", restricted, (req, res, next) => {
+    if (req.headers.authorization) {
+        bc.hash(req.headers.authorization, 8, (err, hash) => {
+            // 2^10 is the number of rounds
+            if (err) {
+                res.status(500).json({ oops: "it broke" });
+            } else {
+                res.status(200).json({ hash });
+            }
+        });
+    } else {
+        res.status(400).json({ error: "missing header" });
+    }
+});
+
+router.post("/register", (req, res) => {
+    let user = req.body;
+
+    const hash = bc.hashSync(req.body.password, 8);
+
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+router.post("/login", (req, res) => {
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bc.compareSync(password, user.password)) {
+                // if (user) {
+                // compare().then(match => {
+                //   if (match) {
+                //     // good password
+                //   } else {
+                //     // they don't match
+                //   }
+                // }).catch()
+                res.status(200).json({ message: `Welcome ${user.username}!` });
+            } else {
+                res.status(401).json({ message: "Invalid Credentials" });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+// Custom validation MW...
+
+function restricted(req, res, next) {
+    // we'll read the username and password from headers
+    // the client is responsible for setting those headers
+    const { username, password } = req.headers;
+  
+    // no point on querying the database if the headers are not present
+    if (username && password) {
+      Users.findBy({ username })
+        .first()
+        .then(user => {
+          if (user && bcrypt.compareSync(password, user.password)) {
+            next();
+          } else {
+            res.status(401).json({ message: 'Invalid Credentials' });
+          }
+        })
+        .catch(error => {
+          res.status(500).json({ message: 'Unexpected error' });
+        });
+    } else {
+      res.status(400).json({ message: 'No credentials provided' });
+    }
+  } 
+
+module.exports = router;
